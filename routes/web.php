bf16e2e83c9fb8f4e4165e4de516276800c01651
Kirill Monakhov <withewings0@gmail.com>
2026-02-17 22:29:41 +0300

    Updated web-routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Http\Controllers\FeedController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [FeedController::class, 'index'])->name('feed.index');
